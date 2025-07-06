7629ccae3d82c8edab70a72a14b063a2502bc01b
Edgar1579 <es535965@gmail.com>
2025-07-05 22:09:33 -0500

    agregue bootstrap icons y carpeta media

--- base/settings.py
+++ base/settings.py
@@ -22,7 +22,9 @@ INSTALLED_APPS = [
     'operaciones',
     'django_bootstrap5',  # pip install django-bootstrap5
     'crispy_forms',  # pip install django-crispy-forms
+    'django_bootstrap_icons',  # pip install django-bootstrap-icons
 ]
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
