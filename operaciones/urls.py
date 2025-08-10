ec1219b1f28431bc98f91d26f2e03a531083aadc
Edgar1579 <es535965@gmail.com>
2025-08-09 22:01:18 -0500

    add anuncios index

--- operaciones/urls.py
+++ operaciones/urls.py
@@ -1,8 +1,9 @@
 # operaciones/urls.py
 from django.urls import path
-from .views import anuncio_lista, anuncio_crear, anuncio_editar, anuncio_eliminar
+from .views import anuncio_lista, anuncio_crear, anuncio_editar, anuncio_eliminar,index
 
 urlpatterns = [
+    path('', index, name='index'),  # Página de inicio
     path('anuncios/', anuncio_lista, name='anuncio_lista'),
     path('anuncios/nuevo/', anuncio_crear, name='anuncio_crear'),
     path('anuncios/editar/<int:id>/', anuncio_editar, name='anuncio_editar'),
